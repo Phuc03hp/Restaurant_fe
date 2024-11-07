f3aebd860be126749be0f22a78528bf54fc78582
TungYeah <minhtung2108@gmail.com>
2024-11-07 23:28:05 +0700

    first commit

---
+++ controller/userCreateAccount.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const signupForm = document.querySelector('.form-container.sign-up form');
+   
+
+
+    signupForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const username = signupForm.querySelector('input[name="username"]').value;
+        const name = signupForm.querySelector('input[name="name"]').value;
+        const password = signupForm.querySelector('input[name="password"]').value;
+
+        try {
+            const response = await fetch('http://127.0.0.1:8081/restaurant/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password, name })
+            });
+
+            if (!response.ok) {
+                const errorResult = await response.json();
+                throw new Error(`Tài khoản tồn tại: ${errorResult.message || response.statusText}`);
+            }
+
+            const result = await response.json();
+            alert('Đăng ký thành công!');
+
+          
+            localStorage.setItem('userID', result.userID);  
+            localStorage.setItem('name', result.name); 
+            localStorage.setItem('username', username);
+
+        } catch (error) {
+            console.error('Lỗi đăng ký:', error);
+            alert(error.message);
+        }
+    });
+})
+ 
+   
